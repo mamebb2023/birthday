50bb7bdafe7b24aa5a9be57b1528428465bba23c
Mohammednur <mamebb2023@gmail.com>
2024-06-12 20:48:55 +0300

    Update

--- src/components/First.jsx
+++ src/components/First.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Section from "./Section";
+import "./First.css";
 
 const First = () => {
   return (
-    <Section className="bg-n-8 text-n-1">
-      <div>
-        <img src="/iphone.png" alt="iphone" />
-        <p className="body-2">for better expirence, flip the laptop</p>
+    <Section className="flex justify-center items-center bg-n-8 text-n-1">
+      <div clasn>
+        <img src="/iphone.png" className="iphone" alt="iphone" />
+        <p className="body-2">For better expirence, Flip the laptop</p>
       </div>
     </Section>
   );
