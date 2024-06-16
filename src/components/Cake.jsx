6f19c6536026df8ced7c7152a61cf1d566871606
Mohammednur <mamebb2023@gmail.com>
2024-06-16 11:32:21 +0300

    Update

--- src/components/Cake.jsx
+++ src/components/Cake.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Section from "./Section";
+import Bake from "./assets/Bake";
+// import cake from "./assets/cake.svg";
 
 const Cake = () => {
-  return <div>Cake</div>;
+  return (
+    <Section className="bg-[#ffda9a] flex justify-center items-center flex-col">
+      <div className="">
+        <div className="cake">
+          <Bake />
+        </div>
+        hello
+      </div>
+    </Section>
+  );
 };
 
 export default Cake;
